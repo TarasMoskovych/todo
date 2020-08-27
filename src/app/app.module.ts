600c97a0b462db2ee93e6b970c895901e4117de2
Taras_Moskovych <taras_moskovych@epam.com>
2020-08-27 12:17:29 +0300

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { CoreModule } from './core/core.module';
+import { MaterialModule } from './material/material.module';
+import { CategoriesModule } from './categories/categories.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    MaterialModule,
+    CategoriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
